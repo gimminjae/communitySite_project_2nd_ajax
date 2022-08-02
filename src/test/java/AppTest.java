0274198c0f776263a8f63e6632fcff8140393f19
gimminjae <min356812@naver.com>
2022-08-03 00:04:55 +0900

    map과 list를 json화 하는 테스트 케이스 작성

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,6 +2,11 @@ import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
@@ -30,5 +35,29 @@ public class AppTest {
         ArticleDto resultArticleDto = Ut.json.toObj(jsonStr, ArticleDto.class, null);
 
         assertThat(resultArticleDto).isEqualTo(articleDto);
+    }
+    @Test
+    void Test_ObjectMapper_mapToJsonStr() {
+        Map<String, ArticleDto> articleDtoMap = new HashMap<>();
+        articleDtoMap.put("가장 오래된", new ArticleDto(1, "title1", "body1"));
+        articleDtoMap.put("최신의", new ArticleDto(2, "title2", "body2"));
+
+        String jsonStr = Ut.json.toJsonStr(articleDtoMap, "");
+
+        assertThat(jsonStr).isEqualTo("""
+                {"가장오래된":{"id":1,"title":"제목1","body":"내용1"},"최신의":{"id":2,"title":"제목2","body":"내용2"}}
+                """.trim());
+    }
+    @Test
+    void Test_ObjectMapper_listToJsonStr() {
+        List<ArticleDto> articleDtoList = new LinkedList<>();
+        articleDtoList.add(new ArticleDto(1, "title1", "body1"));
+        articleDtoList.add(new ArticleDto(2, "title2", "body2"));
+
+        String jsonStr = Ut.json.toJsonStr(articleDtoList, "");
+
+        assertThat(jsonStr).isEqualTo("""
+                [{"id":1,"title":"제목1","body":"내용1"},{"id":2,"title":"제목2","body":"내용2"}]
+                """.trim());
     }
 }
