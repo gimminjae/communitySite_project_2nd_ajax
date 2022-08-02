dbdab92ea0c07562a74038682c98a8220143fd3a
gimminjae <min356812@naver.com>
2022-08-02 23:00:39 +0900

    jackson도입, ObjectMapper(toJsonStr)테스트

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,3 +1,5 @@
+import com.ll.exam.article.dto.ArticleDto;
+import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,5 +10,15 @@ public class AppTest {
         int rs = 10 + 30;
 
         assertThat(rs).isEqualTo(40);
+    }
+    @Test
+    void Test_ObjectMapper() throws Exception {
+        ArticleDto articleDto = new ArticleDto(1, "title1", "body1");
+
+        String rs = Ut.json.toJsonStr(articleDto, "");
+
+        assertThat(rs).isEqualTo("""
+                {"id":1,"title":"title1","body":"body1"}
+                """.trim());
     }
 }
